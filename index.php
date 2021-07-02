01b7dc4b979bbf7de17eff11c7c377c51ee0ac67
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2021-07-02 17:25:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-    $bytes = disk_free_space(".");
-    $si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
-    $base = 1024;
-    $class = min((int)log($bytes , $base) , count($si_prefix) - 1);
-    echo $bytes . '<br />';
-    echo sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
-?>
+$ts = disk_total_space(".");
+$fs = disk_free_space(".");
+
+echo "Total Space: ".hz($ts)."<br>Available Space: ".hz($fs);
+
+
+function hz($bytes, $dec = 2) {
+    $size   = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+    $factor = floor((strlen($bytes) - 1) / 3);
+
+    return sprintf("%.{$dec}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+}
